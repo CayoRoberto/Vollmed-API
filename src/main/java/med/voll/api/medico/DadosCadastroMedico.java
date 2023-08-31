b2ec32c229e0ca4dca76afbbe366b394907d624b
CayoRoberto <cayoroberto08@gmail.com>
2023-08-30 22:04:45 -0300

    Cadastro, atualizacao, delete de medicos

---
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,29 @@
+package med.voll.api.medico;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(
+
+        @NotBlank // verifica se não ta vazio e não nulo e só serve para campos Strings
+        String nome,
+
+        @NotBlank @Email // valida se é um email
+        String email,
+
+        @NotBlank String telefone,
+
+        @NotBlank @Pattern(regexp = "\\d{4,6}") // verifica que são digitos e que podem ser 4 ou 6 digitos
+        String crm,
+
+        @NotNull // não pode ser null
+        Especialidade especialidade,
+
+        @NotNull @Valid // verificar outro DTO
+        DadosEndereco endereco) {
+
+}
---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,71 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+// Lombok
+@Getter // gerar metodos getters
+@NoArgsConstructor // gerar o contrutor default
+@AllArgsConstructor // gerar um construtor que recebe todos os cantos
+@EqualsAndHashCode(of = "id") // gerar o equal hash code apenas no id
+public class Medico {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String nome;
+  private String email;
+  private String telefone;
+  private String crm;
+
+  @Enumerated(EnumType.STRING)
+  private Especialidade especialidade;
+
+  @Embedded
+  private Endereco endereco;
+
+  private Boolean ativo;
+
+  public Medico(DadosCadastroMedico dados) {
+    this.ativo = true;
+    this.nome = dados.nome();
+    this.email = dados.email();
+    this.telefone = dados.telefone();
+    this.crm = dados.crm();
+    this.especialidade = dados.especialidade();
+    this.endereco = new Endereco(dados.endereco());
+
+  }
+
+  public void atualizarInformacoes(@Valid DadosAtualizacaoMedico dados) {
+    if (dados.nome() != null) {
+      this.nome = dados.nome();
+    }
+
+    if (dados.telefone() != null) {
+      this.telefone = dados.telefone();
+    }
+    if (dados.endereco() != null) {
+      this.endereco.atualizarInformacoes(dados.endereco());
+    }
+
+  }
+
+  public void excluir() {
+    this.ativo = false;
+  }
+}
